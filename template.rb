a0023dec6569dcfadf0428700a5942ecd6224662
René Hansen <renehh@gmail.com>
2022-06-16 22:27:43 +0200

    Adds basic template

---
+++ template.rb
@@ -0,0 +1,10 @@
+# template.rb
+generate(:scaffold, "person name:string")
+route "root to: 'people#index'"
+rails_command("db:migrate")
+
+after_bundle do
+  git :init
+  git add: "."
+  git commit: %Q{ -m 'Initial commit' }
+end
